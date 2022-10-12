ac24da27ce69d61d79897355cdcd1d31a1015de9
jordipbou <jordipbou@protonmail.com>
2022-10-13 00:39:10 +0200

    Memory block with init, shrink_up, take and put functions

--- src/dodo.c
+++ src/dodo.c
@@ -11,4 +11,21 @@ void main() {
 	} else {
 		debug_block(b);
 	}
+
+	pair_t *p = take(b);
+	p->car = 13;
+	p->cdr = 17;
+	printf("Taken %p from free list\n", p);
+
+	int err = shrink_up(b, 3);
+	if (err == -1 ) {
+		printf ("HERE is not at last free pair, is not possible to grow continuous region\n");
+	} else if (err == -2) {
+		printf ("There are not enough contiguous pairs to grow continuous region\n");
+	} else {
+		debug_block(b);
+	}
+
+	put(b, p);
+	debug_block(b);
 }
--- src/vm.h
+++ src/vm.h
@@ -17,6 +17,35 @@ typedef struct pair_s {
 #define LAST_FREE_PAIR(b)			b[2]
 #define HERE(b)								b[3]
 
+int shrink_up(cell_t *cells, cell_t amount) {
+	// Ensure last_free_pair is on HERE, if not, it means
+	// that its not possible to take more cells.
+	if (HERE(cells) != LAST_FREE_PAIR(cells)) return -1;
+
+	// Allocates from HERE removing as much pairs from
+	// free list as needed.
+	cell_t required_pairs = amount / 2 + amount % 2;
+
+	// Check if there are enough free cells contiguous to
+	// remove.
+	pair_t *p = (pair_t *)LAST_FREE_PAIR(cells);
+	for (cell_t i = 0; i < required_pairs; i++) {
+		if ((pair_t *)p->car != p + 1) {
+			// There are not enough contiguous free cells starting at HERE
+			return -2;
+		} else {
+			p = (pair_t *)p->car;
+		}
+	}
+
+	// Remove the required cells
+	p->cdr = (cell_t)NULL;
+	HERE(cells) += required_pairs * sizeof(pair_t);
+	LAST_FREE_PAIR(cells) = (cell_t)p;
+
+	return 0;
+}
+
 cell_t *init(cell_t *cells, cell_t size) {
 	if (size % 2 != 0) return NULL;
 
@@ -42,6 +71,28 @@ cell_t *init(cell_t *cells, cell_t size) {
 	return cells;
 }	
 
+// Takes one pair from the beginning of list of free pairs
+pair_t *take(cell_t *cells) {
+	if ((pair_t *)FIRST_FREE_PAIR(cells) == NULL) return NULL;
+
+	pair_t *pair = (pair_t *)FIRST_FREE_PAIR(cells);
+	FIRST_FREE_PAIR(cells) = pair->cdr;
+	((pair_t *)FIRST_FREE_PAIR(cells))->car = (cell_t)NULL;
+
+	pair->car = 0;
+	pair->cdr = 0;
+
+	return pair;
+}
+
+void put(cell_t *cells, pair_t *pair) {
+	pair_t *first = (pair_t *)FIRST_FREE_PAIR(cells);
+	FIRST_FREE_PAIR(cells) = (cell_t)pair;
+	first->car = (cell_t)pair;
+	pair->car = (cell_t)NULL;
+	pair->cdr = (cell_t)first;
+}
+
 // TMP: Debug
 void debug_block(cell_t *cells) {
 	pair_t *pairs = (pair_t *)cells;
@@ -49,10 +100,9 @@ void debug_block(cell_t *cells) {
 	printf("HEADER ----------------------------------------------- (%p)\n", cells);
 	printf("[Size: %ld] [First free: %p] [Last free: %p] [ ]\n", BLOCK_CELLS_SIZE(cells), (pair_t *)FIRST_FREE_PAIR(cells), (pair_t *)LAST_FREE_PAIR(cells));
 	printf("CONTIGUOUS REGION ------------------------------------ (%p)\n", &pairs[HEADER_SIZE]);
-	printf("[HERE: %p]\n", (cell_t *)HERE(cells));
-	printf("MANAGED REGION---------------------------------------- (%p)\n", (pair_t *)LAST_FREE_PAIR(cells));
-	for (cell_t i = HEADER_SIZE; i < BLOCK_PAIRS_SIZE(cells); i++) {
-		printf("[%p] FREE PAIR: PREV %p NEXT %p\n", (pair_t *)&pairs[i], (pair_t *)pairs[i].car, (pair_t *)pairs[i].cdr);
+	printf("MANAGED REGION - HERE: (%p) - LAST FREE CELL: %p\n", (pair_t *)HERE(cells), (pair_t *)LAST_FREE_PAIR(cells));
+	for (pair_t *p = (pair_t *)HERE(cells); p < (pair_t *)(cells + BLOCK_CELLS_SIZE(cells)); p++) {
+		printf("[%p] FREE PAIR: PREV %p NEXT %p\n", p, (pair_t *)p->car, (pair_t *)p->cdr);
 	}
 	printf("END OF MEMORY----------------------------------------- (%p)\n", cells + BLOCK_CELLS_SIZE(cells));
 }
