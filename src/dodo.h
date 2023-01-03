ea31d09aa2a28f13f4c674e14011e8bb8aaee9ac
jordipbou <jordipbou@protonmail.com>
2023-01-03 01:04:37 +0100

    Everything is being implemented around cons/reclaim/allot now.

--- src/dodo.h
+++ src/dodo.h
@@ -1,92 +1,52 @@
 #include<stdint.h>
 
-typedef int8_t BYTE;
-typedef intptr_t CELL;		// Data stack item's size (could be 16, 32 or 64 bits)
+typedef int8_t		BYTE;
+typedef intptr_t	CELL;
 
-typedef struct {
-	CELL car;					// First item, also next on lists
-	CELL cdr;					// Second item, also prev on free stack
+typedef struct _PAIR {
+	struct _PAIR* next;
+	CELL value;
 } PAIR;
 
 typedef struct {
-	CELL status;								// Status and error codes
-	CELL bottom, here;					// Pointers to lowest and highest allocated bytes
-	CELL there, top;						// Pointers to lowest and highest nodes
-	CELL dstack, free, rstack;	// Pointers to data, free and return stacks
-	CELL dict;									// Pointer to last defined word
-	CELL Ax;
+	BYTE* bottom, * here;
+	PAIR* there, * top;
+	PAIR* dstack, * free, * rstack;
 } CTX;
 
 typedef void (*FUNC)(CTX*);
 
-#define S_OK								0
+#define NIL									0
+#define ALIGN(addr, bound)	((CELL)addr + ((CELL)bound-1)) & ~((CELL)bound-1)
+
+#define TYPE(pair)					(((CELL)pair->next) & 3)
+#define UNTYPED(addr)				(PAIR*)(((CELL)addr) & -4)
+PAIR* typed(PAIR* p, CELL t) { p->next = (PAIR*)((CELL)UNTYPED(p->next) | t); return p; }
 
 #define T_FREE							0
-#define T_LIT								1
+#define T_NUM								1
 #define T_WORD							2
-#define T_QUOT							3
-
-#define NIL									0
-
-#define ALIGN(addr, bound)	((CELL)addr + ((CELL)bound-1)) & ~((CELL)bound-1)
+#define T_LIST							3
 
 CTX* init(BYTE* block, CELL size) {
 	CTX* ctx = (CTX*)block;	
-	ctx->bottom = ctx->here = (CELL)((BYTE*)ctx) + sizeof(CTX);
-	ctx->there = (CELL)ALIGN(((BYTE*)ctx) + sizeof(CTX), sizeof(PAIR));
-	ctx->top = (CELL)ALIGN((((BYTE*)ctx) + size) - sizeof(PAIR) - 1, sizeof(PAIR));
+	ctx->bottom = ctx->here = ((BYTE*)ctx) + sizeof(CTX);
+	ctx->there = (PAIR*)(ALIGN(ctx->here, sizeof(PAIR)));
+	ctx->free = ctx->top = (PAIR*)(ALIGN(block + size - sizeof(PAIR) - 1, sizeof(PAIR)));
 
-	for (CELL i = ctx->there; i <= ctx->top; i += sizeof(PAIR)) {
-		PAIR* p = (PAIR*)i;
-		p->car = i == ctx->there ? NIL : i - sizeof(PAIR);
-		p->cdr = i == ctx->top ? NIL : i + sizeof(PAIR);
+	for (PAIR* p = ctx->there; p <= ctx->top; p++) {
+		p->next = p == ctx->there ? NIL : p - 1;
+		p->value = (CELL)(p == ctx->top ? NIL : p + 1);
 	}
 
-	ctx->dstack = ctx->rstack = ctx->dict = NIL;
-	ctx->free = ctx->top;
-
-	ctx->status = S_OK;
+	ctx->dstack = NIL;
 
 	return ctx;
 }
 
-CELL depth(CELL p) { 
-	CELL c = 0; 
-	while (1) { 
-		if (p == 0) { return c; } 
-		else { c++; p = ((PAIR*)p)->car & -4; } 
-	}
-}
+#define RESERVED(ctx)			(((CELL)ctx->there) - ((CELL)ctx->here))
 
-CELL cons(CTX* ctx, CELL car, CELL type, CELL cdr) {
-	PAIR* p = (PAIR*)ctx->free;
-	ctx->free = ((PAIR*)ctx->free)->car & -4;
-	p->car = car | type;
-	p->cdr = cdr;
-	return (CELL)p;
-}
-
-void push(CTX* ctx, CELL n) { ctx->dstack = cons(ctx, ctx->dstack, T_LIT, n); }
-void rpush(CTX* ctx, CELL n) { ctx->rstack = cons(ctx, ctx->rstack, T_LIT, n); }
-
-CELL reclaim(CTX* ctx, CELL* src) {
-	PAIR* p = (PAIR*)*src;
-	CELL v = p->cdr;
-	CELL n = p->car & -4;
-	p->car = ctx->free;
-	((PAIR*)ctx->free)->cdr = (CELL)p;
-	ctx->free = (CELL)p;
-	*src = n;
-	return v;
-}
-
-CELL pop(CTX* ctx) { return reclaim(ctx, &ctx->dstack); }
-CELL rpop(CTX* ctx) { return reclaim(ctx, &ctx->rstack); }
-
-#define RESERVED(ctx)			(ctx->there - ctx->here)
-
-void _allot(CTX* ctx) {
-	CELL bytes = pop(ctx);
+void allot(CTX* ctx, CELL bytes) {
 	if (bytes == 0) return;
 	if (bytes < 0) {
 		if ((ctx->here - bytes) > ctx->bottom) ctx->here -= bytes;
@@ -94,9 +54,9 @@ void _allot(CTX* ctx) {
 		// TODO: Available pairs should be returned to end of free doubly linked list !!
 	} else {
 		while (RESERVED(ctx) < bytes) {
-			if ((((PAIR*)ctx->there)->car & 3) == T_FREE) {
-				((PAIR*)(((PAIR*)ctx->there)->cdr))->car = ((PAIR*)ctx->there)->car;
-				ctx->there += sizeof(PAIR);	
+			if (TYPE(ctx->there) == T_FREE) {
+				((PAIR*)ctx->there->value)->next = ctx->there->next;
+				ctx->there++;
 			} else {
 				// ERROR: Not enough contiguous nodes to free (Not enough memory)
 			}
@@ -105,153 +65,106 @@ void _allot(CTX* ctx) {
 	}
 }
 
-//void _node_align(CTX* c) { c->here = c->there; }
-//
-//// TODO: align 
-//
-//#define T(c)								T_NODE(c)->value
-//#define S(c)								S_NODE(c)->value
-//
-//void _dup(CTX* c) { push(c, T(c)); }
-//void _swap(CTX* c) { CELL t = S(c); S(c) = T(c); T(c) = t; }
-//
-//void _add(CTX* c) { CELL x = pop(c); T(c) += x; }
-//
-//void _dec(CTX* c) { T(c)--; }
-//
-//void _gt(CTX* c) { CELL x = pop(c); T(c) = T(c) > x; }
-//
-//void _header(CTX* c) {
-//	CELL name_length = pop(c);
-//	BYTE* name_addr = (BYTE*)pop(c);
-//
-//	node_align(c);
-//
-//	NODE* name = NODE_AT(c, c->here);
-//	push(c, sizeof(NODE)); allot(c);
-//
-//	name->next = NIL;
-//	name->type = T_ARRAY;
-//	name->len = name_length;
-//
-//	push(c, name_length - 6);
-//	allot(c);
-//
-//	node_align(c);
-//
-//	for (CELL i = 0; i < ((((BYTE*)c) + c->here) - &name->data); i++) {
-//		if (i < name_length) { name->data[i] = name_addr[i]; }
-//		else { name->data[i] = 0; }
-//	}
-//
-//	NODE* word = NODE_AT(c, c->here);
-//	push(c, sizeof(NODE)); allot(c);
-//	word->next = NIL;			// Will point to c->dict after reveal
-//	word->type = T_DICT;
-//	word->name = REL(name);
-//	word->code = NIL;
-//
-//	// Data will start after word node, but must be allocated
-//}
-//
-////void cons(CTX* c, HALF next) {
-////	// TODO: Takes one node from free stack (the one previous to stack)
-////	// TODO: Sets next and type to T_LIST
-////	// TODO: Pushes address to stack
-////}
-////
-////void forget(CTX* c, HALF addr) {
-////	// TODO: Returns nodes recursively (takes type into account) to free stack
-////}
-////
-////// Primitives
-////
-////// TODO: Check stack errors ?!
-////
-////void _add(CTX* c) { CELL x = pop(c); T(c).value += x; }
-////void _sub(CTX* c) { CELL x = pop(c); T(c).value -= x; }
-////void _mul(CTX* c) { CELL x = pop(c); T(c).value *= x; }
-////void _div(CTX* c) { CELL x = pop(c); T(c).value /= x; }
-////void _mod(CTX* c) { CELL x = pop(c); T(c).value %= x; }
-////
-////void _dec(CTX* c) { T(c).value--; }
-////void _inc(CTX* c) { T(c).value++; }
-////
-////void _gt(CTX* c) { CELL x = pop(c); T(c).value = T(c).value > x; }
-////void _lt(CTX* c) { CELL x = pop(c); T(c).value = T(c).value < x; }
-////void _eq(CTX* c) { CELL x = pop(c); T(c).value = T(c).value == x; }
-////void _neq(CTX *c) { CELL x = pop(c); T(c).value = T(c).value != x; }
-////
-////void _dup(CTX* c) { push(c, T(c).value); }
-////void _swap(CTX* c) { CELL t = S(c).value; S(c).value = T(c).value; T(c).value = t; }
-////
-////void _ret(CTX* c) { c->status = S_RET; }
-////
-////void _store(CTX* c) { CELL i = pop(c); CELL x = pop(c); *((CELL*)(((BYTE*)c) + i)) = x; }
-////void _fetch(CTX* c) { CELL i = pop(c); push(c, *((CELL*)(((BYTE*)c) + i))); }
-////
-////void _comma(CTX* c) { 
-////	CELL x = pop(c); 
-////	HALF here = c->here;
-////	allot(c, sizeof(CELL));
-////	*((CELL*)(c + here)) = x;
-////}
-////
-////// Definition creation
-////
-////void _header(CTX* c, BYTE* name, HALF type) {
-////	//align2(c);
-////	NODE* word = (((NODE*)c) + c->here);
-////	allot(c, sizeof(NODE));
-////	word->next = c->dict;	
-////	word->type = type;
-////	word->len = strlen(name);
-////	// TODO: Name is fixed to less than 6 here
-////	for (CELL i = 0; i < 6; i++) {
-////		if (i < word->len) {
-////			word->data[i] = name[i];
-////		} else {
-////			word->data[i] = 0;
-////		}
-////	}
-////}
-////
-////// Inner interpreter
-////
-////// DOUBT: Right now, I'm not using a return stack, as every call is made
-////// directly in C, is that correct? Can a standard FORTH be implemented
-////// that way?
-////// It seems the only real problem will be with control flow words. Only!
-////
-////void _execute(CTX*c, NODE* word);
-////
-////#define DFA(w)		((CELL*)(ALIGN(w + sizeof(CELL) + w->len - 6, sizeof(CELL))))
-////
-////void _docol(CTX* c, CELL* dfa) {
-////	NODE* nodes = (NODE*)c;
-////	HALF* word = (HALF*)dfa;	
-////	while (c->status == S_OK) {
-////		_execute(c, &NODE_AT(c, *word));
-////		word++;
-////	}
-////}
-////
-////void _dovar(CTX* c, CELL* dfa) {
-////	push(c, (CELL)dfa);
-////}
-////
-////void _doconst(CTX* c, CELL* dfa) {
-////	push(c, *dfa);
-////}
-////
-////void _execute(CTX* c, NODE* word) {
-////	if (word->type && T_F_PRIMITIVE) {
-////		((FUNC)(DFA(word)))(c);
-////	} else if (word->type && T_F_DOCOL) { 
-////		_docol(c, DFA(word));
-////	} else if (word->type && T_F_DOVAR) {
-////		_dovar(c, DFA(word));
-////	} else if (word->type && T_F_DOCONST) {
-////		_doconst(c, DFA(word));
-////	}
-////}
+CELL depth(PAIR* p) {
+	CELL c = 0; 
+	while (1) { 
+		if (p == NIL) { return c; } 
+		else { c++; p = UNTYPED(p->next); }
+	}
+}
+
+PAIR* cons(CTX* ctx, PAIR* next, CELL value) {
+	PAIR* p = ctx->free;
+	ctx->free = p->next;
+	p->next = next;
+	p->value = value;
+	return p;
+}
+
+PAIR* reclaim(CTX* ctx, PAIR* p) {
+	if (p == NIL) return NIL;				
+	PAIR* tail = UNTYPED(p->next);
+	p->next = ctx->free;
+	p->value = NIL;
+	ctx->free->value = (CELL)p;
+	ctx->free = p;
+	return tail;
+}
+
+#define BINOP(name, op) \
+	PAIR* name(CTX* ctx, PAIR* list) { \
+		PAIR* tail = UNTYPED(list->next); \
+		tail->value = tail->value op list->value; \
+		return reclaim(ctx, list); \
+	} \
+	void _##name(CTX* ctx) { ctx->dstack = name(ctx, ctx->dstack); }
+
+BINOP(add, +);
+BINOP(sub, -);
+BINOP(mul, *);
+BINOP(div, /);
+BINOP(mod, %);
+
+BINOP(gt, >);
+BINOP(lt, <);
+BINOP(eq, ==);
+BINOP(neq, !=);
+BINOP(gte, >=);
+BINOP(lte, <=);
+
+BINOP(and, &&);
+BINOP(or, ||);
+
+// TODO: How to deal with input buffer?
+
+#define W_PRIMITIVE					1
+#define W_COLON_DEFINITION	3
+
+#define F_NON_IMMEDIATE			1
+#define F_IMMEDIATE					3
+
+#define NFA(w)		((PAIR*)(w->value))
+#define CFA(w)		(UNTYPED(NFA(w)->next))
+#define XT(w)			((PAIR*)(CFA(w)->value))
+#define DFA(w)		(UNTYPED(CFA(w)->next))
+
+PAIR* create(CTX* ctx, BYTE* name, CELL nlen) {
+	BYTE* here = ctx->here;
+
+	allot(ctx, sizeof(CELL) + nlen + 1);
+	*((CELL*)here) = nlen;
+	for (CELL i = 0; i < nlen; i++) {
+		here[sizeof(CELL) + i] = name[i];	
+	}
+	here[sizeof(CELL) + nlen] = 0;
+
+	// TODO: DFA should be CELL aligned !!
+
+	PAIR* dfa = typed(cons(ctx, NIL, (CELL)ctx->here), T_NUM);
+	// TODO: body should be [PUSH DFA] [RET] NIL
+	PAIR* xt = typed(cons(ctx, NIL, dfa->value), T_NUM);
+	PAIR* cfa = typed(cons(ctx, dfa, (CELL)xt), W_COLON_DEFINITION);
+	PAIR* nfa = typed(cons(ctx, cfa, (CELL)here), F_NON_IMMEDIATE);
+	PAIR* word = cons(ctx, NIL, (CELL)nfa);
+
+	return typed(word, T_WORD);
+}
+
+void inner(CTX* ctx, PAIR* list) {
+	while(list != NIL) {
+		if (TYPE(list) == T_NUM) {
+			ctx->dstack = typed(cons(ctx, ctx->dstack, list->value), T_NUM);
+			list = UNTYPED(list->next);
+		} else if (TYPE(list) == T_WORD) {
+			if (TYPE(CFA(list)) == W_PRIMITIVE) {
+				((FUNC)(CFA(list)->value))(ctx);
+				list = UNTYPED(list->next);
+			} else {
+				ctx->rstack = typed(cons(ctx, ctx->rstack, (CELL)(UNTYPED(list->next))), T_NUM);
+				list = (PAIR*)(CFA(list)->value);
+			}
+		} else if (TYPE(list) == T_LIST) {
+			// TODO: execute or push the quotation?
+		}
+	}
+}
--- src/tests.c
+++ src/tests.c
@@ -12,133 +12,436 @@ void test_block_initialization() {
 
 	TEST_ASSERT_EQUAL_INT(ctx->bottom, ((BYTE*)ctx) + sizeof(CTX));
 	TEST_ASSERT_EQUAL_INT(ctx->there, ALIGN(ctx->here, sizeof(PAIR)));
-	TEST_ASSERT_EQUAL_INT(NIL, ((PAIR*)ctx->top)->cdr);
-	TEST_ASSERT_EQUAL_INT(NIL, ((PAIR*)ctx->there)->car);
-	TEST_ASSERT_EQUAL_INT(NIL, ctx->dstack);
+	TEST_ASSERT_EQUAL_INT(NIL, ((PAIR*)ctx->top)->value);
+	TEST_ASSERT_EQUAL_INT(NIL, ((PAIR*)ctx->there)->next);
 	TEST_ASSERT_EQUAL_INT(ctx->top, ctx->free);
-	TEST_ASSERT_EQUAL_INT(NIL, ctx->rstack);
-	TEST_ASSERT_EQUAL_INT(NIL, ctx->dict);
-	TEST_ASSERT_EQUAL_INT(S_OK, ctx->status);
 }
 
-void test_free() {
+void test_free_depth() {
 	CELL size = 256;
 	BYTE block[size];
 	CTX* ctx = init(block, size);
 
 	CELL free_nodes = (size - sizeof(CTX)) / sizeof(PAIR);
-	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
-}
-
-void test_stack() {
-	CELL size = 2048;
-	BYTE block[size];
-	CTX* ctx = init(block, size);
-
-	CELL free_nodes = ((size - sizeof(CTX)) / sizeof(PAIR));
 
 	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
-	TEST_ASSERT_EQUAL_INT(0, depth(ctx->dstack));
-	push(ctx, 13);
-	TEST_ASSERT_EQUAL_INT(free_nodes - 1, depth(ctx->free));
-	TEST_ASSERT_EQUAL_INT(1, depth(ctx->dstack));
-	push(ctx, 17);
-	TEST_ASSERT_EQUAL_INT(free_nodes - 2, depth(ctx->free));
-	TEST_ASSERT_EQUAL_INT(2, depth(ctx->dstack));
-	CELL val = pop(ctx);
-	TEST_ASSERT_EQUAL_INT(free_nodes - 1, depth(ctx->free));
-	TEST_ASSERT_EQUAL_INT(1, depth(ctx->dstack));
-	TEST_ASSERT_EQUAL_INT(17, val);
-	val = pop(ctx);
-	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
-	TEST_ASSERT_EQUAL_INT(0, depth(ctx->dstack));
-	TEST_ASSERT_EQUAL_INT(13, val);
 }
 
 void test_allot() {
-	CELL size = 2048;
+	CELL size = 256;
 	BYTE block[size];
 	CTX* ctx = init(block, size);
 
-	CELL free_nodes = ((size - sizeof(CTX)) / sizeof(PAIR));
+	CELL free_nodes = (size - sizeof(CTX)) / sizeof(PAIR);
 
 	TEST_ASSERT_EQUAL_INT(0, ctx->here - ctx->bottom);
 	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
-	push(ctx, 3);
-	_allot(ctx);
+	allot(ctx, 3);
 	TEST_ASSERT_EQUAL_INT(3, ctx->here - ctx->bottom);
 	TEST_ASSERT_EQUAL_INT(ctx->bottom + 3 + RESERVED(ctx), ctx->there);
-	push(ctx, 13);
-	_allot(ctx);
+	allot(ctx, 13);
 	TEST_ASSERT_EQUAL_INT(16, ctx->here - ctx->bottom);
-	push(ctx, 1);
-	_allot(ctx);
+	allot(ctx, 1);
 	TEST_ASSERT_EQUAL_INT(17, ctx->here - ctx->bottom);
 	TEST_ASSERT_LESS_THAN(free_nodes, depth(ctx->free));
 }
 
-////void test_store_and_fetch() {
-////	CELL size = 2048;
-////	BYTE block[size];
-////	CTX* c = init(block, size);
-////
-////	HALF here = c->here;
-////	allot(c, sizeof(CELL));
-////	push(c, 13);
-////	push(c, here);
-////	_store(c);
-////	TEST_ASSERT_EQUAL_INT(13, *((CELL*)(((BYTE*)c) + here)));
-////	TEST_ASSERT_EQUAL_INT(0, DEPTH(c));
-////	_fetch(c);
-////	TEST_ASSERT_EQUAL_INT(1, DEPTH(c));
-////	TEST_ASSERT_EQUAL_INT(13, pop(c));
-////}
-////
-////void test_header() {
-////	CELL size = 2048;
-////	BYTE block[size];
-////	CTX* c = init(block, size);
-////
-////	HALF here = c->here;
-////	_header(c, "+", T_F_PRIMITIVE);
-////	NODE word = NODE_AT(c, here);
-////	TEST_ASSERT_EQUAL_INT(here + sizeof(NODE), c->here);
-////	TEST_ASSERT_EQUAL_INT(c->dict, word.next);
-////	TEST_ASSERT_EQUAL_INT(T_F_PRIMITIVE, word.type);
-////	TEST_ASSERT_EQUAL_INT(1, word.len);
-////	TEST_ASSERT_EQUAL_INT8('+', word.data[0]);
-////	TEST_ASSERT_EQUAL_INT8(0, word.data[1]);
-////	TEST_ASSERT_EQUAL_INT8(0, word.data[2]);
-////	TEST_ASSERT_EQUAL_INT8(0, word.data[3]);
-////	TEST_ASSERT_EQUAL_INT8(0, word.data[4]);
-////	TEST_ASSERT_EQUAL_INT8(0, word.data[5]);
-////}
-////
-////void test_inner() {
-////	CELL size = 2048;
-////	BYTE block[size];
-////	CTX* c = init(block, size);
-////
-////	push(c, 7);
-////	push(c, 11);
-////	// TODO: Create a word definition for add primitive
-////	// TODO: Execute word and test stack has only the value 18
-////}
+void test_cons() {
+	CELL size = 256;
+	BYTE block[size];
+	CTX* ctx = init(block, size);
+
+	CELL free_nodes = (size - sizeof(CTX)) / sizeof(PAIR);
+
+	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+	PAIR* p = cons(ctx, NIL, 13);
+	TEST_ASSERT_EQUAL_INT(free_nodes - 1, depth(ctx->free));
+	TEST_ASSERT_EQUAL_INT(NIL, p->next);
+	TEST_ASSERT_EQUAL_INT(13, p->value);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	PAIR* p2 = cons(ctx, p, 17);
+	TEST_ASSERT_EQUAL_INT(free_nodes - 2, depth(ctx->free));
+	TEST_ASSERT_EQUAL_INT(p, p2->next);
+	TEST_ASSERT_EQUAL_INT(17, p2->value);
+	TEST_ASSERT_EQUAL_INT(13, p2->next->value);
+	TEST_ASSERT_EQUAL_INT(2, depth(p2));
+}
+
+void test_reclaim() {
+	CELL size = 256;
+	BYTE block[size];
+	CTX* ctx = init(block, size);
+
+	CELL free_nodes = (size - sizeof(CTX)) / sizeof(PAIR);
+
+	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+	PAIR* p = cons(ctx, cons(ctx, cons(ctx, NIL, 13), 17), 21);
+	TEST_ASSERT_EQUAL_INT(3, depth(p));
+	TEST_ASSERT_EQUAL_INT(free_nodes - 3, depth(ctx->free));
+	p = reclaim(ctx, p);
+	TEST_ASSERT_EQUAL_INT(2, depth(p));
+	TEST_ASSERT_EQUAL_INT(free_nodes - 2, depth(ctx->free));
+	p = reclaim(ctx, p);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	TEST_ASSERT_EQUAL_INT(free_nodes - 1, depth(ctx->free));
+	p = reclaim(ctx, p);
+	TEST_ASSERT_EQUAL_INT(0, depth(p));
+	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+	p = reclaim(ctx, p);
+	TEST_ASSERT_EQUAL_INT(0, depth(p));
+	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+}
+
+void test_binops() {
+	CELL size = 256;
+	BYTE block[size];
+	CTX* ctx = init(block, size);
+
+	PAIR* p = cons(ctx, cons(ctx, NIL, 7), 13);
+	TEST_ASSERT_EQUAL_INT(2, depth(p));
+
+	p = add(ctx, p);
+	TEST_ASSERT_EQUAL_INT(p->value, 20);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = sub(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 6);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = mul(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 91);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = div(ctx, cons(ctx, cons(ctx, NIL, 30), 5));
+	TEST_ASSERT_EQUAL_INT(p->value, 6);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = mod(ctx, cons(ctx, cons(ctx, NIL, 9), 2));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = gt(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = gt(ctx, cons(ctx, cons(ctx, NIL, 7), 13));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = gt(ctx, cons(ctx, cons(ctx, NIL, 7), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = lt(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = lt(ctx, cons(ctx, cons(ctx, NIL, 7), 13));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = lt(ctx, cons(ctx, cons(ctx, NIL, 7), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = eq(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = eq(ctx, cons(ctx, cons(ctx, NIL, 7), 13));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = eq(ctx, cons(ctx, cons(ctx, NIL, 7), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = neq(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = neq(ctx, cons(ctx, cons(ctx, NIL, 7), 13));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = neq(ctx, cons(ctx, cons(ctx, NIL, 7), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = gte(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = gte(ctx, cons(ctx, cons(ctx, NIL, 7), 13));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = gte(ctx, cons(ctx, cons(ctx, NIL, 7), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = lte(ctx, cons(ctx, cons(ctx, NIL, 13), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = lte(ctx, cons(ctx, cons(ctx, NIL, 7), 13));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = lte(ctx, cons(ctx, cons(ctx, NIL, 7), 7));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = and(ctx, cons(ctx, cons(ctx, NIL, 0), 0));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = and(ctx, cons(ctx, cons(ctx, NIL, 0), 1));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = and(ctx, cons(ctx, cons(ctx, NIL, 1), 0));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = and(ctx, cons(ctx, cons(ctx, NIL, 1), 1));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+
+	p = or(ctx, cons(ctx, cons(ctx, NIL, 0), 0));
+	TEST_ASSERT_EQUAL_INT(p->value, 0);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = or(ctx, cons(ctx, cons(ctx, NIL, 0), 1));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = or(ctx, cons(ctx, cons(ctx, NIL, 1), 0));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+	p = or(ctx, cons(ctx, cons(ctx, NIL, 1), 1));
+	TEST_ASSERT_EQUAL_INT(p->value, 1);
+	TEST_ASSERT_EQUAL_INT(1, depth(p));
+	reclaim(ctx, p);
+
+}
+
+void test_create() {
+	CELL size = 256;
+	BYTE block[size];
+	CTX* ctx = init(block, size);
+
+	BYTE* here = ctx->here;
+	PAIR* w = create(ctx, "test", 4);
+	TEST_ASSERT_EQUAL_INT(T_WORD, TYPE(w));
+	TEST_ASSERT_EQUAL_INT(4, *((CELL*)(NFA(w)->value)));
+	TEST_ASSERT_EQUAL_STRING("test", ((BYTE*)NFA(w)->value) + sizeof(CELL));
+	TEST_ASSERT_EQUAL_INT(here + sizeof(CELL) + 4 + 1, DFA(w)->value);
+	TEST_ASSERT_EQUAL_INT(here + sizeof(CELL) + 4 + 1, XT(w)->value);
+}
+
+void test_inner_interpreter() {
+	CELL size = 256;
+	BYTE block[size];
+	CTX* ctx = init(block, size);
+
+}
 
 int main() {
 	UNITY_BEGIN();
 
 	RUN_TEST(test_block_initialization);
-	RUN_TEST(test_free);
-	RUN_TEST(test_stack);
+
+	RUN_TEST(test_free_depth);
 	RUN_TEST(test_allot);
+	RUN_TEST(test_cons);
+	RUN_TEST(test_reclaim);
+	RUN_TEST(test_binops);
 
+	RUN_TEST(test_create);
 
-	//RUN_TEST(test_store_and_fetch);
-
-	//RUN_TEST(test_header);
-
-	//RUN_TEST(test_inner);
+	RUN_TEST(test_inner_interpreter);
 
 	return UNITY_END();
 }
+
+//void setUp() {}
+//
+//void tearDown() {}
+//
+//void test_block_initialization() {
+//	CELL size = 256;
+//	BYTE block[size];
+//	CTX* ctx = init(block, size);
+//
+//	TEST_ASSERT_EQUAL_INT(ctx->bottom, ((BYTE*)ctx) + sizeof(CTX));
+//	TEST_ASSERT_EQUAL_INT(ctx->there, ALIGN(ctx->here, sizeof(PAIR)));
+//	TEST_ASSERT_EQUAL_INT(NIL, ((PAIR*)ctx->top)->cdr);
+//	TEST_ASSERT_EQUAL_INT(NIL, ((PAIR*)ctx->there)->car);
+//	TEST_ASSERT_EQUAL_INT(NIL, ctx->dstack);
+//	TEST_ASSERT_EQUAL_INT(ctx->top, ctx->free);
+//	TEST_ASSERT_EQUAL_INT(NIL, ctx->rstack);
+//	TEST_ASSERT_EQUAL_INT(NIL, ctx->dict);
+//	TEST_ASSERT_EQUAL_INT(S_OK, ctx->status);
+//}
+//
+//void test_free() {
+//	CELL size = 256;
+//	BYTE block[size];
+//	CTX* ctx = init(block, size);
+//
+//	CELL free_nodes = (size - sizeof(CTX)) / sizeof(PAIR);
+//	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+//}
+//
+//void test_stack() {
+//	CELL size = 2048;
+//	BYTE block[size];
+//	CTX* ctx = init(block, size);
+//
+//	CELL free_nodes = ((size - sizeof(CTX)) / sizeof(PAIR));
+//
+//	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+//	TEST_ASSERT_EQUAL_INT(0, depth(ctx->dstack));
+//	push(ctx, 13);
+//	TEST_ASSERT_EQUAL_INT(free_nodes - 1, depth(ctx->free));
+//	TEST_ASSERT_EQUAL_INT(1, depth(ctx->dstack));
+//	//push(ctx, 17);
+//	//TEST_ASSERT_EQUAL_INT(free_nodes - 2, depth(ctx->free));
+//	//TEST_ASSERT_EQUAL_INT(2, depth(ctx->dstack));
+//	//CELL val = pop(ctx);
+//	//TEST_ASSERT_EQUAL_INT(free_nodes - 1, depth(ctx->free));
+//	//TEST_ASSERT_EQUAL_INT(1, depth(ctx->dstack));
+//	//TEST_ASSERT_EQUAL_INT(17, val);
+//	//val = pop(ctx);
+//	//TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+//	//TEST_ASSERT_EQUAL_INT(0, depth(ctx->dstack));
+//	//TEST_ASSERT_EQUAL_INT(13, val);
+//}
+//
+//void test_allot() {
+//	CELL size = 2048;
+//	BYTE block[size];
+//	CTX* ctx = init(block, size);
+//
+//	CELL free_nodes = ((size - sizeof(CTX)) / sizeof(PAIR));
+//
+//	TEST_ASSERT_EQUAL_INT(0, ctx->here - ctx->bottom);
+//	TEST_ASSERT_EQUAL_INT(free_nodes, depth(ctx->free));
+//	push(ctx, 3);
+//	_allot(ctx);
+//	TEST_ASSERT_EQUAL_INT(3, ctx->here - ctx->bottom);
+//	TEST_ASSERT_EQUAL_INT(ctx->bottom + 3 + RESERVED(ctx), ctx->there);
+//	push(ctx, 13);
+//	_allot(ctx);
+//	TEST_ASSERT_EQUAL_INT(16, ctx->here - ctx->bottom);
+//	push(ctx, 1);
+//	_allot(ctx);
+//	TEST_ASSERT_EQUAL_INT(17, ctx->here - ctx->bottom);
+//	TEST_ASSERT_LESS_THAN(free_nodes, depth(ctx->free));
+//}
+//
+//////void test_store_and_fetch() {
+//////	CELL size = 2048;
+//////	BYTE block[size];
+//////	CTX* c = init(block, size);
+//////
+//////	HALF here = c->here;
+//////	allot(c, sizeof(CELL));
+//////	push(c, 13);
+//////	push(c, here);
+//////	_store(c);
+//////	TEST_ASSERT_EQUAL_INT(13, *((CELL*)(((BYTE*)c) + here)));
+//////	TEST_ASSERT_EQUAL_INT(0, DEPTH(c));
+//////	_fetch(c);
+//////	TEST_ASSERT_EQUAL_INT(1, DEPTH(c));
+//////	TEST_ASSERT_EQUAL_INT(13, pop(c));
+//////}
+//////
+//////void test_header() {
+//////	CELL size = 2048;
+//////	BYTE block[size];
+//////	CTX* c = init(block, size);
+//////
+//////	HALF here = c->here;
+//////	_header(c, "+", T_F_PRIMITIVE);
+//////	NODE word = NODE_AT(c, here);
+//////	TEST_ASSERT_EQUAL_INT(here + sizeof(NODE), c->here);
+//////	TEST_ASSERT_EQUAL_INT(c->dict, word.next);
+//////	TEST_ASSERT_EQUAL_INT(T_F_PRIMITIVE, word.type);
+//////	TEST_ASSERT_EQUAL_INT(1, word.len);
+//////	TEST_ASSERT_EQUAL_INT8('+', word.data[0]);
+//////	TEST_ASSERT_EQUAL_INT8(0, word.data[1]);
+//////	TEST_ASSERT_EQUAL_INT8(0, word.data[2]);
+//////	TEST_ASSERT_EQUAL_INT8(0, word.data[3]);
+//////	TEST_ASSERT_EQUAL_INT8(0, word.data[4]);
+//////	TEST_ASSERT_EQUAL_INT8(0, word.data[5]);
+//////}
+//////
+//////void test_inner() {
+//////	CELL size = 2048;
+//////	BYTE block[size];
+//////	CTX* c = init(block, size);
+//////
+//////	push(c, 7);
+//////	push(c, 11);
+//////	// TODO: Create a word definition for add primitive
+//////	// TODO: Execute word and test stack has only the value 18
+//////}
+//
+//int main() {
+//	UNITY_BEGIN();
+//
+//	RUN_TEST(test_block_initialization);
+//	RUN_TEST(test_free);
+//	RUN_TEST(test_stack);
+//	//RUN_TEST(test_allot);
+//
+//
+//	//RUN_TEST(test_store_and_fetch);
+//
+//	//RUN_TEST(test_header);
+//
+//	//RUN_TEST(test_inner);
+//
+//	return UNITY_END();
+//}
